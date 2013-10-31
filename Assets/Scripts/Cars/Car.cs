97cc7df14812a5faa06ab302c65206e190cbdd9d
Stefan <lordrajar@yahoo.de>
2013-10-31 18:23:26 +0100

    TestAuto eingefügt
    Das TestAuto ist da, um die zusammenarbeit der einzelnen Komponenten in
    der Hierachie zu testen

--- Assets/Scripts/Cars/Car.cs
+++ Assets/Scripts/Cars/Car.cs
@@ -1,6 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * Diese Klasse enthält alle Autorelevanten Daten. 
+ * Außerdem verarbeitet sie den Input eines Controller Scripts, dies kann ein Spieler oder eine KI sein.
+ *
+ */
+
+
 public class Car : MonoBehaviour {
 	
 	//nur zu testzwcken
@@ -8,8 +15,11 @@ public class Car : MonoBehaviour {
 	public float BreakTorque = 20;
 	public float SteerAngle = 30;
 	
+	
+/*	Momentan nicht in Gebrauch
 	//Motor, Schaltung
-	//Drehmomentkennline des Fahrzeuges. X-Achse entspricht Umdrehungen pro Minute (RPM), Y-Achse enstspricht der daraus resultierende Drehmonent in Newtonmeter
+	//Drehmomentkennline des Fahrzeuges. X-Achse entspricht Umdrehungen pro Minute (RPM), Y-Achse enstspricht der daraus resultierende 
+	//Drehmonent in Newtonmeter
 	public AnimationCurve EngineTorqueCurve;
 	//Effizienz der Schaltung (es entstehen immer Verluste)
 	public float TransmissionEfficiency = 0.7f;
@@ -23,18 +33,50 @@ public class Car : MonoBehaviour {
 	public float CDrag = 0.4f;
 	//Multiplier für seitlichen Luftwiederstand
 	public float sideDragMultiplier = 1.0f;
-	//Zentrum der Masse
+*/
+	//Zentrum der Masse für den RigiBody
 	public Transform CenterOfMass;
 	
 	//Referenzen zu den Reifen
-	public WheelCollider[] wheels;
+	public Wheel[] wheels;
 	
+/*	momentan nicht in gebrauch
+	//WheelFrictionCurves, sollten für alle Reifen gleich sein. Um das nicht ständig ändern zu müssen, wird das an einer zentralen Stelle gepsichert 
+	//und an die Reifen weitergegeben
+	private WheelFrictionCurve forwardFrictionCurve;
+	private WheelFrictionCurve sidewaysFrictionCurve;
+	
+	//Werte im Bezug auf den Controller
+	// Wert für den durchgedrückten Gaspedal, von -1 (rückwärts) bis 1 (vorwärts)
+	private float throttle = 0.0f;
+	//Wert für den Lenker, von -1 bis 1
+	private float steer = 0.0f;
+	//Zeit, wie lange die Kuplung noch nicht drin ist, falls > 0 keine Motorkraft auf Reifen
+	private float gearChange = 0.0f;
+	
+	//Aktuelle Werte
+	//aktueller Gang
+	private int currentGear = 0;
+	//aktuelle Umdrehungen des Motors pro Minute
+	private float currentRPM = 1000;
+	//kann das Auto steuern? (Räder auf dem Boden?)
+	private bool canSteer;
+	//kann das Auto beschleunigen?
+	private bool canDrive;
+*/	
+	void Awake()
+	{
+		
+		
+	}
 	
 	// Use this for initialization
 	void Start () {
 		//Massezemtrum sollte weiter vorne und weiter unten liegen, daher Referenz auf eine anderes in der Hierariche plaziertes Objekt
 		//rigidbody.centerOfMass = CenterOfMass.transform.position;
-	
+		
+		//setForwardFrictionCurve(float asymSlip, float asymValue, float exSlip, float exValue, float stiff);
+
 	}
 	
 	// Update is called once per frame
@@ -57,6 +99,7 @@ public class Car : MonoBehaviour {
 	{
 		//Rollwiderstand
 		//Ground.getRollingResistanceCoefficient(); Rollwiederstandkoefizient ist abhängig vom Untergrund
+		//eventuell kannman auch die Layer bestimmen, auf dem der Wagen fährt
 		float GroundCoefRR = 1f;
 		//Rollwiderstandswert = Rollwiederstandskoeffizient * Masse * Gravitation
 		float CoefRR = GroundCoefRR * rigidbody.mass * 9.81f;
@@ -72,13 +115,14 @@ public class Car : MonoBehaviour {
 	
 	private void getInput()
 	{
+		
 		if(Input.GetKey(KeyCode.UpArrow))
 		{
 			//Gas geben, Drehmoment wird auf reifen übertragen, nur wenen Reifen boden berühren
-			if(wheels[0].isGrounded || wheels[1].isGrounded)
+			if(wheels[0].GetComponent<WheelCollider>().isGrounded || wheels[1].GetComponent<WheelCollider>().isGrounded)
 			{
-				wheels[0].motorTorque = MotorTorque;
-				wheels[1].motorTorque = MotorTorque;
+				wheels[0].GetComponent<WheelCollider>().motorTorque = MotorTorque;
+				wheels[1].GetComponent<WheelCollider>().motorTorque = MotorTorque;
 				//wheels[2].motorTorque = MotorTorque;
 				//wheels[3].motorTorque = MotorTorque;
 			}
@@ -86,22 +130,26 @@ public class Car : MonoBehaviour {
 		
 		if(Input.GetKey(KeyCode.DownArrow))
 		{
-			if(wheels[3].isGrounded || wheels[2].isGrounded)
+			if(wheels[3].GetComponent<WheelCollider>().isGrounded || wheels[2].GetComponent<WheelCollider>().isGrounded)
 			{
-				wheels[2].brakeTorque = BreakTorque;
-				wheels[3].brakeTorque = BreakTorque;
+				wheels[2].GetComponent<WheelCollider>().brakeTorque = BreakTorque;
+				wheels[3].GetComponent<WheelCollider>().brakeTorque = BreakTorque;
 			}
 		}
 		
 		if(Input.GetKey(KeyCode.RightArrow))
 		{
-			wheels[0].steerAngle = SteerAngle;
-			wheels[1].steerAngle = SteerAngle;
+			wheels[0].GetComponent<WheelCollider>().steerAngle = SteerAngle;
+			//wheels[0].wheelGraphic.rotation.y = SteerAngle;
+			wheels[1].GetComponent<WheelCollider>().steerAngle = SteerAngle;
+			//wheels[1].wheelGraphic.rotation.y = SteerAngle;
 		}
 		if(Input.GetKey(KeyCode.LeftArrow))
 		{
-			wheels[0].steerAngle = -SteerAngle;
-			wheels[1].steerAngle = -SteerAngle;
+			wheels[0].GetComponent<WheelCollider>().steerAngle = -SteerAngle;
+			//wheels[0].wheelGraphic.rotation.y = -SteerAngle;
+			wheels[1].GetComponent<WheelCollider>().steerAngle = -SteerAngle;
+			//wheels[1].wheelGraphic.rotation.y = -SteerAngle;
 		}
 		resetTorque();
 		
@@ -110,11 +158,15 @@ public class Car : MonoBehaviour {
 	//alle Werte werden wirder auf 0 gesetzt, damit es zu keiner dauerhaften Beschleunigung kommt
 	private void resetTorque()
 	{
-		foreach(WheelCollider wheel in wheels)
+		float accelSmooth = 26f;
+		float steerSmooth = 9f;
+		
+		foreach(Wheel wheel in wheels)
 		{
-			wheel.brakeTorque = Mathf.Lerp(wheel.brakeTorque, 0f, 0.2f);
-			wheel.motorTorque = Mathf.Lerp(wheel.motorTorque, 0f, 0.2f);
-			wheel.steerAngle = Mathf.Lerp(wheel.steerAngle, 0f, 0.2f);
+			wheel.GetComponent<WheelCollider>().brakeTorque = Mathf.Lerp(wheel.GetComponent<WheelCollider>().brakeTorque, 0f, accelSmooth * Time.deltaTime);
+			wheel.GetComponent<WheelCollider>().motorTorque = Mathf.Lerp(wheel.GetComponent<WheelCollider>().motorTorque, 0f, accelSmooth * Time.deltaTime);
+			wheel.GetComponent<WheelCollider>().steerAngle = Mathf.Lerp(wheel.GetComponent<WheelCollider>().steerAngle, 0f, steerSmooth * Time.deltaTime);
+			//wheel.wheelGraphic.rotation.y = Mathf.Lerp(wheel.wheelGraphic.rotation.y, 0f, 0.2f);
 		}
 	}
 	
---
+++ Assets/Scripts/Cars/Wheel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wheel : MonoBehaviour {
+	
+	//Referent auf GrafikObjekt
+	public Transform wheelGraphic;
+	
+	// Use this for initialization
+	void Start () {
+	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+}
