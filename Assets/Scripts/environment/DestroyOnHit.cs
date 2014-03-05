774222f3e6f27f02e99fcbc32aa8a45197970029
FrankAken <frank.the.krake@gmail.com>
2014-03-05 16:34:24 +0100

    DesertCircuit Update

---
+++ Assets/Scripts/environment/DestroyOnHit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnHit : MonoBehaviour {
+
+	public float delay;
+
+	void OnTriggerEnter(Collider collider){
+		Debug.Log(collider.transform.root.tag);
+		if(collider.transform.root.tag == "Player"){
+			Destroy (gameObject.transform.parent.gameObject,delay);
+		}
+	}
+}
