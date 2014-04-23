90efa0550241efa13dd9bf8d731c1fb7ca460230
Der C <goriar.cb@gmail.com>
2014-04-23 16:07:58 +0200

    Neuer Map marker, der sich mit dem Spieler dreht

--- Assets/Scripts/GUI/MiniMapElement.cs
+++ Assets/Scripts/GUI/MiniMapElement.cs
@@ -20,7 +20,8 @@ public class MiniMapElement : MonoBehaviour
 		if(parent != null){
 			Vector3 newPos = new Vector3(parent.transform.position.x,this.transform.position.y,parent.transform.position.z);
 			this.transform.position = newPos;
-			this.transform.rotation = initialRotation;
+			this.transform.rotation = Quaternion.identity;
+			this.transform.Rotate(90.0f,parent.transform.rotation.eulerAngles.y,0.0f,Space.Self);
 		}
 	}
 
