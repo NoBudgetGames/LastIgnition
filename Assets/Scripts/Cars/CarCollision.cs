836872f1eedb6e12dee2eb5fe14d11e0aca4ca37
Der C. <goriar.cb@gmail.com>
2014-04-25 17:48:18 +0200

    Crash sound spielt nur bei erhöter geschwindigkeit

--- Assets/Scripts/Cars/CarCollision.cs
+++ Assets/Scripts/Cars/CarCollision.cs
@@ -27,10 +27,12 @@ public class CarCollision : MonoBehaviour
 	//Waffen werden von diesen Trigger nicht erkannt, da sie in einer anderen Layer liegen (layer-base collision detection)
 	void OnTriggerEnter(Collider other)
 	{
-		if(Network.connections.Length == 0){
-			GameObject.Instantiate(virtualCrashPrefab,this.transform.position,this.transform.rotation);
-		} else {
-			Network.Instantiate(virtualCrashPrefab,this.transform.position,this.transform.rotation,0);
+		if(car.GetComponent<Rigidbody>().velocity.magnitude > 10.0f){
+			if(Network.connections.Length == 0){
+				GameObject.Instantiate(virtualCrashPrefab,this.transform.position,this.transform.rotation);
+			} else {
+				Network.Instantiate(virtualCrashPrefab,this.transform.position,this.transform.rotation,0);
+			}
 		}
 		//falls other ein DestructibleCarPart hat
 		if(other.GetComponent<DestructibleCarPart>())
