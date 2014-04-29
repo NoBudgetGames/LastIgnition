412b54bb6399a6cc5b2b3618562e259ca5371048
Der C <goriar.cb@gmail.com>
2014-04-29 18:52:28 +0200

    Rocket fix
    Von OntTriggerEnter auf OnColissionEnter, außerdem Geschwindigkeit
    verringert

--- Assets/Scripts/Weapons/Rocket.cs
+++ Assets/Scripts/Weapons/Rocket.cs
@@ -24,13 +24,14 @@ public class Rocket : MonoBehaviour
 		damage = 2.5f;
 		radius = 10.0f;
 		force = 7000.0f;
-		speed = 500.0f;
+		speed = 250.0f;
 	}
 
 	void FixedUpdate ()
 	{
 		//Simpler gerader Bahnverlauf
 		transform.position += forwardVector*Time.deltaTime*speed;
+		//this.rigidbody.AddForce(forwardVector*speed,ForceMode.Acceleration);
 
 		destroyTimer -= Time.deltaTime;
 		//falls der Timer abgelaufen ist, zerstöre die Rakete
@@ -39,7 +40,7 @@ public class Rocket : MonoBehaviour
 			GameObject.Destroy(gameObject);
 		}
 	}
-	
+	/*
 	void OnTriggerEnter(Collider other){
 		if(other.GetComponent<Checkpoint>())
 			return;
@@ -47,8 +48,48 @@ public class Rocket : MonoBehaviour
 		if(other.GetComponent<SpawnZone>())
 		{
 			return;
-		}			
-
+		}
+		if(parent.transform.root == other.transform.root){
+			return;
+		}
+	
+		GameObject.Instantiate(explosionPrefab,this.transform.position,this.transform.rotation);
+		//Von der Explosionsposition der Rakete aus werden mittels einer Sphere alle Objekte
+		//im Explosionsradius erfasst. Objekte die zerstörbar sind erhalten Schaden, Objekte 
+		//mit einem Rigidbody werden zurückgeworfen (abhängig von der Masse)
+		Vector3 explosionPosition = transform.position;
+		Collider[] affectedObjects = Physics.OverlapSphere(explosionPosition,radius);
+		foreach(Collider col in affectedObjects){
+			if(col.rigidbody)
+				col.rigidbody.AddExplosionForce(force,explosionPosition,radius);
+			AbstractDestructibleObject destr = col.GetComponent<AbstractDestructibleObject>();
+			if(destr != null)
+			{
+				destr.receiveDamage(damage);
+				if(Network.connections.Length > 0){
+					col.networkView.RPC("receiveDamage",col.networkView.owner,damage);
+				}
+				if(col.GetComponent<DestructibleCarPart>())
+				{
+					col.GetComponent<DestructibleCarPart>().car.rigidbody.AddExplosionForce(force * 2,explosionPosition,radius);
+				}
+			}				
+		}
+		GameObject.Destroy(this.gameObject);
+	}
+*/
+	void OnCollisionEnter(Collision collision) {
+		if(collision.collider.GetComponent<Checkpoint>())
+			return;
+		//falls wir eine SpawnZone getroffen haben, mache nichts
+		if(collision.collider.GetComponent<SpawnZone>())
+		{
+			return;
+		}
+		if(parent.transform.root == collision.collider.transform.root){
+			return;
+		}
+		
 		GameObject.Instantiate(explosionPrefab,this.transform.position,this.transform.rotation);
 		//Von der Explosionsposition der Rakete aus werden mittels einer Sphere alle Objekte
 		//im Explosionsradius erfasst. Objekte die zerstörbar sind erhalten Schaden, Objekte 
